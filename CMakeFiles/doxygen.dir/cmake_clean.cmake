08bbc092e15251216b8216c95e2d1cef1f5aee68
thilinarmtb <thilinarmtb@users.noreply.github.com>
2024-10-21 17:47:27 +0000

    Deploying to gh-pages from  @ f2b0ded64a7b41c0c8db7f1425347410eb5bd8fa 🚀

--- CMakeFiles/doxygen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/doxygen"
-  "doxygen/xml/index.xml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/doxygen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
